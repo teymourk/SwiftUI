87a84945cbe5772e14a2fb82b9bda54e34eb62a5
Kia <teymourk@oregonstate.edu>
2019-10-14 17:18:49 -0700

    Initial Commit

---
+++ GetStartedUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GetStartedUI
+//
+//  Created by Kiarash on 10/14/19.
+//  Copyright © 2019 Kiarash. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
